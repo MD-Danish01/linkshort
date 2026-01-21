fe7e853edda91061e652d7865cf9028cf972fe5a
DANISH <mdastudies84478@gmail.com>
2026-01-21 13:10:39 +0530

    change db

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb";
 export async function POST(request) {
   try {
     const client = await clientPromise;
-    const db = client.db("bitlinkproject");
+    const db = client.db("LinkShortenerDB");
     const collection = db.collection("urls");
     const { longUrl, shortCode } = await request.json();
     //check if shortCode already exists
