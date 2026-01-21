d0fced64700983dda3d99bf4ce07e3cc88df7963
Md Danish <220763213+MD-Danish01@users.noreply.github.com>
2026-01-21 20:02:48 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <section>
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
           <h1 className="md:text-6xl text-2xl font-bold">Welcome to URL Shortener</h1>
-          <p className="mt-3 md:text-2xl text-xl">Shorten your links easily and quickly</p>
+          <p className="mt-3 md:text-2xl text-xl">Shorten your links easily and quickly.</p>
           <Link href={"/shorten"}>
             <button className="mt-6 px-6 py-3 bg-sky-500 text-white rounded-2xl">
               Get Started
